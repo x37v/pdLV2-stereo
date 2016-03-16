8889acc6b238cfe46691fdeaf011a06f54581572
Alex Norman <x37v.alex@gmail.com>
2016-03-16 08:16:06 -0700

    cleaning up ruby parser, printing out info

--- cpp/parse.rb
+++ cpp/parse.rb
@@ -1,30 +1,34 @@
 require 'rdf'
 require 'linkeddata'
 
-plugins = ["patch.pd"]
-
-controlInRegex = /r(?:eceive){0,1}\s*\\\$0-lv2-(.*);\s*/
-controlOutRegex = /s(?:end){0,1}\s*\\\$0-lv2-(.*);\s*/
+@controlInRegex = /r(?:eceive){0,1}\s*\\\$0-lv2-(.*);\s*/
+@controlOutRegex = /s(?:end){0,1}\s*\\\$0-lv2-(.*);\s*/
+@controlLabelRegex = /label:\s*(\w*)/
+@floatRegex = /\d+(?:\.\d+)?/
+@rangeRegex = /range:\s+(#{@floatRegex})\s*(#{@floatRegex})\s*(#{@floatRegex})/
 
 def get_control_data(content)
-  floatRegex = /\d+(?:\.\d+)?/
-  controlLabelRegex = /label:\s*(\w*)/
-  rangeRegex = /range:\s+(#{floatRegex})\s*(#{floatRegex})\s*(#{floatRegex})/
-
   data = {}
   data[:symbol] = content.match(/\A(\w+)/)[0]
   data[:label] =
-    if content =~ controlLabelRegex
+    if content =~ @controlLabelRegex
       $1
     end
   data[:range] = 
-    if content =~ rangeRegex
-      [$1, $3, $2]
+    if content =~ @rangeRegex
+      [$1, $2, $3]
     end
   return data
 end
 
-plugins.each do |p|
+def print_control(data)
+  r = data[:range]
+  puts "\t#{data[:symbol]}"
+  puts "\t\tlabel: #{data[:label]}" if data[:label]
+  puts "\t\trange: #{r.join(', ')}" if r
+end
+
+def parse_pd_file(patch_path)
   input = 0
   output = 0
   name = nil
@@ -32,7 +36,7 @@ plugins.each do |p|
   in_controls = []
   out_controls = []
 
-  File.open(p) do |f|
+  File.open(patch_path) do |f|
     f.readlines.each do |l|
       if l =~ /dac~\s(.*);\s*/
         $1.scan(/\d+/).each do |d|
@@ -42,9 +46,9 @@ plugins.each do |p|
         $1.scan(/\d+/).each do |d|
           input = d.to_i if d.to_i > input
         end
-      elsif l =~ controlInRegex
+      elsif l =~ @controlInRegex
         in_controls << get_control_data($1)
-      elsif l =~ controlOutRegex
+      elsif l =~ @controlOutRegex
         out_controls << get_control_data($1)
       elsif l =~ /pluginURI:\s(.*);\s*/
         uri = $1
@@ -55,12 +59,30 @@ plugins.each do |p|
 
     raise "need uri" unless uri
     raise "need name" unless name
+    raise "need at least one control or audio input or output" unless input + output + in_controls.size + out_controls.size > 0
+
 
     puts "name: #{name}"
     puts "uri: #{uri}"
     puts "audio inputs: #{input}"
     puts "audio outputs: #{output}"
-    puts "control inputs: #{in_controls.size}"
-    puts "control outputs: #{out_controls.size}"
+    if in_controls.size
+      puts "control inputs:"
+      in_controls.each do |c|
+        print_control(c)
+      end
+    end
+    if out_controls.size
+      puts "control outputs:"
+      out_controls.each do |c|
+        print_control(c)
+      end
+    end
   end
+end
+
+
+plugins = ["patch.pd"]
+plugins.each do |p|
+  parse_pd_file(p)
 end
