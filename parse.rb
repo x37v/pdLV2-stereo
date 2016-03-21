64f00028b312c0f4c147a98cd2cd9eea78b2ca61
Alex Norman <x37v.alex@gmail.com>
2016-03-21 08:12:02 -0700

    added license to ruby code

--- parse.rb
+++ parse.rb
@@ -1,4 +1,24 @@
 #!/usr/bin/env ruby
+=begin
+
+Copyright (c) Alex Norman, 2016.
+
+This file is part of pdlv2.
+
+pdlv2 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pdlv2 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pdlv2.  If not, see <http://www.gnu.org/licenses/>.
+
+=end
 
 require 'rdf'
 require 'rdf/ntriples'
