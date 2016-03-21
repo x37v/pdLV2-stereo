dd2ee6c58d797c234ef9a3ef6e0d1804d1d02a78
Alex Norman <x37v.alex@gmail.com>
2016-03-21 08:31:49 -0700

    removed old project, copied license, need to figure that out

--- old/getURIs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#grep -o "pluginURI:.*;" $1 | sed s/";$"/""/ | sed s/"pluginURI: "/"templateURI "/ | sed s/"\/"/"\\\\\/"/g > $2
-grep -o "pluginURI:.*;" $1 | sed s/";$"/""/ | sed s/"pluginURI: "/"templateURI "/ | sed s/"[\/\.\:]"/"\\\\\\\\\0"/g > $2
